c9377454de4e3dfeda92883363449e68a51af955
Jacob Gadikian <faddat@gmail.com>
2016-01-26 03:50:34 +0700

    added the file verification for safety

--- example/coreos.sh
+++ example/coreos.sh
@@ -3,6 +3,10 @@
 mkdir coreos
 rm coreos/coreos_production_pxe.vmlinuz
 rm coreos/coreos_production_pxe_image.cpio.gz
-wget -O coreos/coreos.vmlinux http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
-wget -O coreos/coreos.gz http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
+wget -O coreos/coreos_production_pxe.vmlinuz http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
+wget -O coreos/coreos_production_pxe.vmlinuz.sig http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz.sig
+wget -O coreos/coreos_production_pxe_image.cpio.gz http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
+wget -O coreos/coreos_production_pxe_image.cpio.gz.sig http://beta.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz.sig
+gpg --verify coreos/coreos_production_pxe.vmlinuz.sig
+gpg --verify coreos/coreos_production_pxe_image.cpio.gz.sig
 pixiecore -kernel coreos_production_pxe.vmlinuz -initrd coreos_production_pxe_image.cpio.gz --cmdline coreos.autologin
